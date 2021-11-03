905bc946b4e6ba45ff89a94947672b82ce109ce1
Shawn Yang <dhryxy@gmail.com>
2021-11-03 10:50:27 +0800

    New translations common.dart (Dutch)

--- lib/utils/i18n/nl/common.dart
+++ lib/utils/i18n/nl/common.dart
@@ -13,6 +13,7 @@ const Map<String, String> enCommon = {
   'list.end': 'No More Data',
   'address': 'Send to Address',
   'amount': 'Amount',
+  'amount.max': 'Max',
   'amount.available': 'Available',
   'amount.error': 'Invalid amount',
   'amount.low': 'Insufficient balance',
@@ -20,6 +21,7 @@ const Map<String, String> enCommon = {
   'amount.all': 'Total',
   'amount.staked': 'Staked',
   'amount.free': 'Free',
+  'amount.transferable': 'Transferable',
   'input.invalid': 'Invalid input',
   'balance': 'Balance',
   'currency': 'Currency',
@@ -33,33 +35,21 @@ const Map<String, String> enCommon = {
   'loan.title': 'Mint aUSD',
   'loan.brief':
       'Lock your digital assets(i.e. DOT) in Acala platform to borrow equivalent aUSD.',
-  'loanKSM.title': 'Mint kUSD',
-  'loanKSM.brief':
-      'Lock your digital assets(i.e. KSM) in Acala platform to borrow equivalent kUSD.',
   'swap.title': 'Swap',
   'swap.brief':
       'Decentralized token exchange with the liquid pool of the Acala platform.',
-  'swapKSM.title': 'Swap',
-  'swapKSM.brief':
-      'Decentralized token exchange with the liquid pool of the Karura platform.',
   'earn.title': 'Earn',
   'earn.brief':
-      'Deposit your digital assets and stable coins into the liquid pool to earn interests and rewards',
-  'earnKSM.title': 'Earn',
-  'earnKSM.brief':
       'Deposit your digital assets and stable coins into the liquid pool to earn interests and rewards',
   'homa.title': 'Liquid DOT',
   'homa.brief':
       'Deposit DOT & Mint Liquid DOT (L-DOT). Your DOTs will be staked to earn returns, meanwhile you can use L-DOT in your wallet.',
-  'homaKSM.title': 'Liquid KSM',
-  'homaKSM.brief':
-      'Deposit KSM & Mint Liquid KSM (L-KSM). Your KSMs will be staked to earn returns, meanwhile you can use L-KSM in your wallet.',
   'nft.title': 'NFTs',
   'nft.brief': 'NFT collectables.',
-  'nftKSM.title': 'NFTs',
-  'nftKSM.brief': 'NFT collectables.',
   'coming': 'Await governance activation',
   'asset.lock': 'Locked',
   'asset.reserve': 'Reserved',
   'asset.transferable': 'Transferable',
+  'action.disable':
+      'action is disabled due to runtime upgrade, try again later.',
 };
