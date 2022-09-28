e8eabc268a39215ef8a67222b930f3698e3b4dc3
shawn <dhryxy@gmail.com>
2022-09-28 14:47:42 +0800

    remove OnFinality from node list

--- lib/common/constants/nodeList.dart
+++ lib/common/constants/nodeList.dart
@@ -31,11 +31,6 @@ const node_list = [
     'ss58': ss58_prefix_acala,
     'endpoint': 'wss://acala-rpc-3.aca-api.network/ws',
   },
-  {
-    'name': 'Acala (via OnFinality)',
-    'ss58': ss58_prefix_acala,
-    'endpoint': 'wss://acala-polkadot.api.onfinality.io/public-ws',
-  },
   // {
   //   'name': 'Acala (acala dev node)',
   //   'ss58': ss58_prefix_acala,
